b587e58355f581104ddcb5a6904fe44b28c3c6fb
filipswat <31748796+filipswat@users.noreply.github.com>
2021-01-25 16:27:14 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,34 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("event1");
+        events.add("event2");
+        events.add("event3");
+        events.add("event4");
+
+        model.addAttribute("events", events);
+
+        return "events/index";
+    }
+
+    // due to request mapping on class, lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+}
